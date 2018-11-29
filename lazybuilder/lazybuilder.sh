9eff4804ef4c39a85b2469fe16c7d65a1844b3a3
Firewolf <firewolfxda@outlook.com>
2018-11-29 17:06:04 +0600

    Add colorrrrrrrrrrrrrrrr....

--- lazybuilder/lazybuilder.sh
+++ lazybuilder/lazybuilder.sh
@@ -4,17 +4,36 @@
 #slpooy28112018
 #
 # firewolfxda@outlook.com
-echo
-echo "Please choose build type : "
-echo
-read -p "
-  1. Clean
-  2. Dirty
-  3. Quit
+#
+blue='\033[0;34m'
+cyan='\033[0;36m'
+green='\e[0;32m'
+yellow='\033[0;33m'
+nocol='\033[0m'
+purple='\e[0;35m'
+white='\e[0;37m'
+red='\033[0;31m'
 
-  : " lzbtype1
 echo
-read -p "Please enter device codename : " lzbdevice1
+
+printf "${blue} Please choose build type :${nocol}"
+
+echo
+
+printf "\n
+${cyan}  1. Clean ${nocol}\n
+${yellow}  2. Dirty ${nocol}\n
+${red}  0. Exit ${nocol}\n
+\n"
+
+read -p "  : " lzbtype1
+if [ "$lzbtype1" = "0" ] || [ "$lzbtype1" = "1" ] || [ "$lzbtype1" = "2" ]; then
+echo
+if [ "$lzbtype1" = "0" ]; then
+                exit
+fi
+printf "${blue} Please enter device codename: ${nocol}"
+read -p "" lzbdevice1
 echo
 if [ "$lzbtype1" = "1" ]; then
                  make clean
@@ -25,7 +44,6 @@ if [ "$lzbtype1" = "1" ]; then
                  lunch omni_$lzbdevice1-eng
                  mka recoveryimage
                  echo
-
 elif [ "$lzbtype1" = "2" ]; then
                  export ALLOW_MISSING_DEPENDENCIES=true
                  export LC_ALL=C
@@ -34,8 +52,10 @@ elif [ "$lzbtype1" = "2" ]; then
                  mka recoveryimage
                  echo
 
-elif [ "$lzbtype1" = "3" ]; then
+elif [ "$lzbtype1" = "0" ]; then
                 exit
+fi
 else
-  printf "Invalid value\n\n"
+  printf "${red}\n Invalid value\n${nocol}${red} Try again or exit...\n${nocol}"
+  bash lazybuilder.sh
 fi
